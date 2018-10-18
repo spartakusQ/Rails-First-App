47c402a2c5fb4b90c973f52ad1f56e846f592332
Unknown <spartakus1688@gmail.com>
2018-10-18 13:04:53 +0300

    Add some fix after code review

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,16 @@
 class Test < ApplicationRecord
 
+  has_many :passing_tests
+  has_many :users, through: :passing_tests
+
   def self.title_by_category
     by_category(title).order(title: :desc).map(&:title)
   end
 
+  def by_level
+
+  end
+  
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  has_many :passing_tests
+  has_many :tests, through: :passing_tests
+
 
   def tests_by_level(level)
-    tests.tests_by_level(level)
+    tests.by_level
   end
-  
+
+
+
 end
