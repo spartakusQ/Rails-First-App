3aea9024cea418ad60ed4dfca8dacde008603c11
Unknown <spartakus1688@gmail.com>
2018-10-22 22:07:14 +0300

    Add associations for models.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,15 @@
-# frozen_string_literal: true
-
 class Test < ApplicationRecord
+
   belongs_to :category
   has_many :passing_tests
   has_many :users, through: :passing_tests
+  has_many :questions
+  belongs_to :author, class_name: 'User'
 
   def self.titles_by_category(title)
     join(:category).where(categories: { title: title }).order(title: :desc).pluck(:title)
   end
 end
 
-# Client.where(active: true).pluck(:id)
+
+
