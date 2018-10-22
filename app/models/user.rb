ff85de9e1f7264dd3b7b402a6f7c50153cd55939
Unknown <spartakus1688@gmail.com>
2018-10-22 16:43:28 +0300

    Fix association to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 class User < ApplicationRecord
   has_many :passing_tests
-  has_many :tests
+  has_many :tests, through: :passing_tests
 
   def tests_by_level(level)
     tests.where(level: level)
