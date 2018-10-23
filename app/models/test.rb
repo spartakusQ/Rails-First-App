bfc972542619ffbd8cf495387afaa840fc8116a6
Unknown <spartakus1688@gmail.com>
2018-10-23 22:21:56 +0300

    Add scope method to test model (titles_by_category)

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,13 +5,16 @@ class Test < ApplicationRecord
   has_many :users, through: :passing_tests
   has_many :questions
   belongs_to :author, class_name: 'User'
-
+  
   scope :easy, -> { by_level(0..1) }
   scope :medium, -> { by_level(2..4) }
   scope :hard, -> { by_level(5..Float::INFINITY) }
+  scope :by_level, ->(level) { where(level: level) }
+  scope :by_category, ->(title) { joins(:category).where(categories: { title: title }) }
+
 
   def self.titles_by_category(title)
-    join(:category).where(categories: { title: title }).order(title: :desc).pluck(:title)
+    by_category(title).order(title: :desc).pluck(:title)
   end
 end
 
