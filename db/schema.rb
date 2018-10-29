f982e08f4df220c9f63fde16541f50d28de58da8
Unknown <spartakus1688@gmail.com>
2018-10-29 21:55:41 +0300

    Add migration AddIndexToUser

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_065640) do
+ActiveRecord::Schema.define(version: 2018_10_29_184814) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2018_10_29_065640) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
