1ab69fa151d22ae6a3b2d2396a977d9b3963cfb6
Unknown <spartakus1688@gmail.com>
2018-10-29 10:42:11 +0300

    Add user authentification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
+require "digest/sha1"
+
 class User < ApplicationRecord
+
+  include Auth  
 
   has_many :passing_tests
   has_many :tests, through: :passing_tests
   has_many :tests_author, class_name: 'Test', foreign_key: :author_id
-  
-  validates :email, presence: true
+
+  has_secure_password
 
   def tests_by_level(level)
     tests.by_level(level)
@@ -13,8 +17,6 @@ class User < ApplicationRecord
   def passing_test(test)
     passing_test.order(id: :desc).find_by(test_id: test.id)
   end
+
+
 end
-
-
-
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_201851) do
+ActiveRecord::Schema.define(version: 2018_10_29_065640) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2018_10_26_201851) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
