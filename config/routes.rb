0ab232ce4d0a065743f34690760e1706b4d1face
Unknown <spartakus1688@gmail.com>
2018-10-29 14:29:58 +0300

    Add User sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
+
   root to: 'tests#index'
 
   get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
+
   resources :users, only: :create
+  resources :sessions, only: :create
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
