55fbf78eae293a497c3dc525d34335b93213dd4b
Unknown <spartakus1688@gmail.com>
2018-10-30 21:51:25 +0300

    Redirecting admins to / admin / tests after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  protected
+
+  def after_sign_in_path_for(user)
+    user.is_a?(Admin) ? admin_tests_path : super
+  end
+
 end
