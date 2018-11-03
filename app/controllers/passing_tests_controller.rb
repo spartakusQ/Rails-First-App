ad4f9b1e0778640c6302e0f91d26c66aeb92a02c
Unknown <spartakus1688@gmail.com>
2018-11-03 19:14:08 +0300

    At the administrator, make a table of all the created gist.

--- app/controllers/passing_tests_controller.rb
+++ app/controllers/passing_tests_controller.rb
@@ -22,20 +22,23 @@ class PassingTestsController < ApplicationController
   end
 
   def gist
-  service = GistQuestionService.new(@passing_test.current_question)
-  response = service.call
+    service = GistQuestionService.new(@passing_test.current_question)
+    response = service.call
 
-  gist_url = response.html_url
-  gist_link = view_context.link_to(gist_url, gist_url, target: :blank)
+    gist_url = response.html_url
+    gist_link = view_context.link_to(gist_url, gist_url, target: :blank)
 
-  flash_option = if service.status_ok? make_gist!(gist_url)
-    { notice: t('.success', url: gist_link) }
-  else
-    { alert: t('.failed') }
+    flash_option = if service.status_ok? make_gist!(gist_url)
+        { notice: t('.success', url: gist_link) }
+      else
+        { alert: t('.failed') }
+      end
+      redirect_to @passing_test, flash_option
   end
 
-  redirect_to @passing_test, flash_option
-end
+
+
+
 
   private
 
@@ -44,7 +47,6 @@ end
   end
 
   def make_gist!(gist_url)
-    current_user.gists.create(question: @test_passing.current_question, url: gist_url)
+    current_user.gists.create(question: @passing_test.current_question, url: gist_url)
   end
-
 end
--- app/models/gist.rb
+++ app/models/gist.rb
@@ -1,4 +1,8 @@
 class Gist < ApplicationRecord
   belongs_to :user
   belongs_to :question
+
+  def gist_hash_url
+    url.split("/").last
+  end
 end
