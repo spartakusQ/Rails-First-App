5c31193b6825d5e6154e44dcc9e99ac8af09f84c
Unknown <spartakus1688@gmail.com>
2018-11-08 14:34:12 +0300

    Support pg database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_03_103522) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "correct", default: false
